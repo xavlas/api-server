a5a47a2e7dafe62c6115ec4cca2860a05d1e26ea
xl <lassus.xavier@gmail.com>
2025-10-25 19:42:23 +0200

    ini

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ import { errorHandler } from './src/middleware/errorHandler.js';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 // Middleware de sécurité
 app.use(helmet());
